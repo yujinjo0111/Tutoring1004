cf2f151414df97e2072c0f5338189ff93beb4b39
yujinjo0111 <78085881+yujinjo0111@users.noreply.github.com>
2021-02-13 03:42:08 +0900

    Create 31.c

---
+++ Week_3_3/31.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct person
+{
+	int number;
+	char cell;
+ double eye;
+
+};
+int main()
+{
+	struct person arr[5];
+	int i;
+	for (i = 0; i < 5; i++)
+	{	printf("학번 : ");
+	scanf_s("%d", &arr[i].number);
+	printf("혈액형 :");
+	scanf_s("%s", arr[i].cell);
+	printf("시력 : ");
+	scnaf_s("%lf", &arr[i].eye);
+    }
+	for (i = 0; i < 5; i++)
+	{
+		printf("학번 %d, 혈액형: %s,시력:%lf", arr[i].number, arr[i].cell, arr[i].eye);
+
+	}
+	return 0;
+
+
+}
+
+
+
+////오류남...
