ff783be2523b83b21fa74f25550546fbf4fbd04b
yujinjo0111 <78085881+yujinjo0111@users.noreply.github.com>
2021-02-13 11:56:10 +0900

    Update 33.c

--- Week_3_3/33.c
+++ Week_3_3/33.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
-struct storage{
+#include <string.h> //이거 쓰기 strcmp작동
+struct storage {
 	char name[20];
 	char number[100];
 
 };
 int main()
 {
-	struct storage people[10];
-	
+	struct storage people[2];
+
 	char input[100];
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 2; i++)
 	{
 		printf("이름 : \n");
-		scanf_s("%s", people[i].name);  //에러에러에러!!!!다시하기
+		scanf("%s", people[i].name);  
 		printf("전화번호 :");
-		scanf_s("%s", people[i].number);
+		scanf("%s", people[i].number);
 	}
-	
-		printf("검색할 사람 이름:");
-	scanf_s("%s", input);
-	for (int i = 0; i < 10; i++)
+
+	printf("검색할 사람 이름:");
+	scanf("%s", input);
+	for (int i = 0; i < 2; i++)
 	{
-		if (strcmp(input, people[i].name) == 0)
-			printf("찾은 번호: %s", people[i].number);
-		else
-			printf("없습니다");
+		if (strcmp(input, people[i].name) == 0) {
+			printf("찾은 번호: %s", people[i].number);    //else로 하지 말고 return printf 
+			return;
+		}
 	}
+	printf("없습니다");
 	return 0;
 
 
